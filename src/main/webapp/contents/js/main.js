0d8ef77ec814a433c30d28a9c0cc6eb90ea42678
ibrahim <smartman402@hotmail.com>
2020-05-10 10:49:19 +0200

    Deploy Heroku ok

--- src/main/webapp/contents/js/main.js
+++ src/main/webapp/contents/js/main.js
@@ -350,7 +350,7 @@ function transaction_money() {
 	};
 
 	// we use sweetalert in order to confirm sending money before you say ohhh :)
-	if (to_BankAccount !== "") {
+	if (to_BankAccount !== "" && amountToSend!==0) {
 		swal({
 			title: 'Are you sure?',
 			text: " you want to send € " + amountToSend + " to account number " + to_BankAccount,
